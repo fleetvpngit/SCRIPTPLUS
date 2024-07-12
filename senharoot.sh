8a69fdd9252670b309fd6b5be5621d2bc8ebc6b0
fleetvpngit <fleetvpn@gmail.com>
2024-07-11 21:29:18 -0300

    Update senharoot.sh

--- senharoot.sh
+++ senharoot.sh
@@ -2,22 +2,26 @@
 clear
 
 [[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
-	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
+    sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
 } > /dev/null
 [[ $(grep -c "without-password" /etc/ssh/sshd_config) != '0' ]] && {
-	sed -i "s/without-password/yes/g" /etc/ssh/sshd_config
+    sed -i "s/without-password/yes/g" /etc/ssh/sshd_config
 } > /dev/null
 [[ $(grep -c "#PermitRootLogin" /etc/ssh/sshd_config) != '0' ]] && {
-	sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
+    sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
 } > /dev/null
 [[ $(grep -c "PasswordAuthentication" /etc/ssh/sshd_config) = '0' ]] && {
-	echo 'PasswordAuthentication yes' > /etc/ssh/sshd_config
+    echo 'PasswordAuthentication yes' > /etc/ssh/sshd_config
 } > /dev/null
 [[ $(grep -c "PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
-	sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+    sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 } > /dev/null
 [[ $(grep -c "#PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
-	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+    sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 } > /dev/null
+[[ $(grep -c "Include /etc/ssh/sshd_config.d/*.conf" /etc/ssh/sshd_config) != '0' ]] && {
+    sed -i "/Include \/etc\/ssh\/sshd_config\.d\/\*\.conf/d" /etc/ssh/sshd_config
+} > /dev/null
+
 service ssh restart > /dev/null
 clear; echo -e "\033[1;32mA SEGUIR DEFINA A SENHA ROOT\033[0m"; sleep 2s; passwd && rm senharoot.sh
